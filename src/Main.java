06214409af0c8047a521cba9edb274e6bc52a8a2
chipallino1 <skorupich00@mail.ru>
2018-02-19 23:02:05 +0300

    New Version Of Project

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import by.bstu.it.skorupich.testFunction;
 public class Main {
     //TODO Method 12
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        testFunction testFun = new testFunction();
+        for(int i=0;i<9;i++)
+            if(i>-1)
+                if(i<10)
+                    System.out.println(i);
+
     }
 }
