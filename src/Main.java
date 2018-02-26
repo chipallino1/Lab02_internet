547af5e31a9b5d5e129fb3a9825f710324a121ac
chipallino1 <skorupich00@mail.ru>
2018-02-26 16:38:40 +0300

    package

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         testFunction testFun = new testFunction();
+
         for(int i=0;i<9;i++)
             if(i>-1)
                 if(i<10)
                     System.out.println(i);
+
+        testFun.PrintByeWorld();
 
     }
 }
---
+++ src/by/bstu/it/skorupich/testFunction.java
@@ -0,0 +1,9 @@
+package by.bstu.it.skorupich;
+
+public class testFunction {
+    public void PrintByeWorld()
+    {
+        System.out.print("Bye World!!!");
+
+    }
+}
